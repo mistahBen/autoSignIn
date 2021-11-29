d581809627b18a7c90443f11a33db759a2b1cec8
Ben Alexander <ben@tuhax.com>
2021-11-29 15:42:38 -0600

    typo

--- signin.py
+++ signin.py
@@ -1,17 +1,17 @@
-g_confrom config import *
-from elements import *
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-import csv
-import os
-from os.path import exists
-import random
-import re
 import time
+import re
+import random
+from os.path import exists
+import os
+import csv
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from elements import *
+from config import *
 
 
 def main():
